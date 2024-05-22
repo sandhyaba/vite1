87bdd91f221a66231133a23474d1826add7c47c7
sandhyaba <86476805+sandhyaba@users.noreply.github.com>
2024-05-22 12:57:18 +0530

    Create Title

---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css'
+
+const Title = ({subTitle, title}) => {
+    return (
+        <div className='title'>
+            <p>{subTitle}</p>
+            <h3>{title}</h3>
+
+        </div>
+    )
+}
+
+export default Title
