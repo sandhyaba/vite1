0c005c2a27e1f7062a324f320470363f21e58b42
sandhyaba <86476805+sandhyaba@users.noreply.github.com>
2024-05-22 12:48:13 +0530

    Create Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+import { Link } from 'react-scroll'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>© 2024,Edusity, All rights reserved.</p>
+      <ul>
+        <li><a href='https://www.termsfeed.com/blog/sample-terms-and-conditions-template/#Terms_And_Conditions_Faqs' target='_blank' rel='noopener noreferrer'>Terms of Services</a></li>
+        <li><a href='https://en.wikipedia.org/wiki/Privacy_policy' target='_blank' rel='noopener noreferrer'>Privacy Policy</a></li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Footer
