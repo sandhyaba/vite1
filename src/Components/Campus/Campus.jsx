dbaf9778a5047cd1aa3c64a6f2151a4a482d3bff
sandhyaba <86476805+sandhyaba@users.noreply.github.com>
2024-05-22 12:44:51 +0530

    Create Campus

---
+++ src/Components/Campus/Campus.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import './Campus.css';
+import Arrow from '../../assets/white-arrow.png';
+
+const Campus = () => {
+  const [userData, setUserData] = useState([]);
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://192.168.1.142:6500/v1/user/message/list');
+        setUserData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChange = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className='campus'>
+      {userData.map((user, index) => (
+        <div className={`gallery ${!expanded && index >= 3 ? 'hidden' : ''}`} key={index}>
+          <img src={user.image} alt={`image-${index}`} className='image' />
+        </div>
+      ))}
+      <div className="see-more-img">
+        <button className='see-btn' onClick={handleChange}>
+          {expanded ? 'See Less' : 'See More Here'}
+          <img src={Arrow} alt="arrow" className='arrow-img' />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Campus;
